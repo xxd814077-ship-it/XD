a975f119ee23565c1e331af8069ae7719af6f361
xxd814077-ship-it <xxd814077@gmail.com>
2026-01-08 17:53:32 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot aktif ve Render üzerinde çalışıyor!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda dinleniyor.`);
+});
+
+const token = process.env.TOKEN;
+const channelId = process.env.CHANNEL_ID;
+const message = process.env.MESSAGE;
+
+if (!token || !channelId || !message) {
+    console.error("HATA: Environment (gizli değişkenler) bölümünde TOKEN, CHANNEL_ID veya MESSAGE eksik!");
+} else {
+  
+    setInterval(sendMessage, 3000);
+}
+
+function sendMessage() {
+  axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, {
+    content: message
+  }, {
+    headers: {
+      "Authorization": token,
+      "Content-Type": "application/json"
+    }
+  }).then(() => {
+    console.log(`✅ Mesaj başarıyla gönderildi: "${message}"`);
+  }).catch((err) => {
+    console.error("❌ Mesaj gönderilemedi. Hata:", err.response?.status, err.response?.data);
+  });
+}
